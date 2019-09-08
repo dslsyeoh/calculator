e95218e68f018f5a1e8787a9b71e17d66abd5125
dslsyeoh <syeoh91x@gmail.com>
2019-09-09 00:30:32 +0800

    refactor demo code

--- src/com/dsl/calculator/CalculatorDemo.java
+++ src/com/dsl/calculator/CalculatorDemo.java
@@ -5,24 +5,27 @@
 
 package com.dsl.calculator;
 
-import static com.dsl.calculator.Operator.*;
-
 public class CalculatorDemo
 {
     public static void main(String[] args)
     {
         Calculator calculator = new Calculator();
-        calculator.setOperator(ADDITION);
-        System.out.println(String.format("%.2f + %.2f = %.2f", 1.5, 3.5, calculator.calculate(1.5, 3.5)));
-        calculator.setOperator(SUBTRACTION);
-        System.out.println(String.format("%.2f - %.2f = %.2f", 4.5, 2.5, calculator.calculate(4.5, 2.5)));
-        calculator.setOperator(MULTIPLICATION);
-        System.out.println(String.format("%.2f * %.2f = %.2f", 3.0, 2.0, calculator.calculate(3, 2)));
-        calculator.setOperator(DIVISION);
-        System.out.println(String.format("%.2f / %.2f = %.2f", 11.0, 2.0, calculator.calculate(11, 2)));
-
-        System.out.println(calculator.chain(ADDITION, 3).chain(MULTIPLICATION, 3).chain(3).calculate());
-        System.out.println(calculator.chain(ADDITION, 1.5, 3.5).chain(MULTIPLICATION, 3).calculate());
-        System.out.println(calculator.chain(ADDITION, 1.5, 3.5).chain(3).calculate());
+        System.out.println(String.format("%.2f + %.2f = %.2f", 1.5, 3.5, calculator.add(1.5, 3.5).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f - %.2f = %.2f", 4.5, 2.5, calculator.subtract(4.5, 2.5).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f * %.2f = %.2f", 3.0, 2.0, calculator.multiply(3, 2).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f / %.2f = %.2f", 11.0, 2.0, calculator.divide(11, 2).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f * %.2f * %.2f = %.2f", 3.0, 3.0, 3.0, calculator.add(3).multiply(3).multiply(3).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f + %.2f * %.2f = %.2f", 1.5, 3.5, 3.0, calculator.add(1.5, 3.5).multiply(3).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f + %.2f + %.2f = %.2f", 1.5, 3.5, 3.0, calculator.add(1.5, 3.5).add(3).getTotal()));
+        calculator.clear();
+        System.out.println(String.format("%.2f + %.2f - %.2f * %.2f / %.2f = %.2f", 10.0, 5.0, 5.0, 3.0, 10.0, calculator.add(10, 5).subtract(5).multiply(3).divide(10).getTotal()));
+        System.out.println(String.format("%.2f + %.2f - %.2f * %.2f / %.2f = %.2f", 10.0, 5.0, 5.0, 3.0, 10.0, calculator.add(10, 5).subtract(5).multiply(3).divide(10).getTotal()));
+        System.out.println(String.format("%.2f + %.2f - %.2f * %.2f / %.2f = %.2f", 10.0, 5.0, 5.0, 3.0, 10.0, calculator.add(10, 5).subtract(5).multiply(3).divide(10).getTotal()));
     }
 }
